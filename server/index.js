ca84ae5a3b2eff87b73f132df0628e34a15a32cf
Dhiren <puridhiren@gmail.com>
2023-05-31 17:25:38 +0100

    first mern commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import {userRouter} from './routes/users.js';
+import {DB_CONNECTION} from './env.js';
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.use("/auth", userRouter);
+
+
+
+mongoose.connect(DB_CONNECTION, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    
+});
+
+
+
+
+
+app.listen(3001,()=>{ console.log("Started on port 3001 !!!") })
